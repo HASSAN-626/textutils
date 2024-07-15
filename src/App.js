b18274257faeea9dfba1823f0a0560f00fedba19
HASSAN-626 <hassansrdar626@gmail.com>
2024-07-15 15:44:00 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -4,53 +4,53 @@ import { useState } from "react";
 function App() {
   return (
     <>
-    <div className="bg-gray-50">
+    <div className="bg-gray-50 ">
       <div className="flex flex-row">
         <div className="px-10 py-20  h-screen border border-gray-900 fixed left-0 font-sans md:font-serif">
           <img src="./download.jpg" alt="" width={85} height={85}/>
-          <a href="" className="p-3">
+          <a href="" className="p-3 font-semibold">
             <p>Dashboard</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>Personal</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
            <p> My Rota </p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>Inbox</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>c2o Profile</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>Chats</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>ATS</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>CMS</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>OCCS</p>
           </a>
-          <a href="" className="p-4">
+          <a href="" className="p-4 font-semibold">
             <p>Preference</p>
           </a>
         </div>
         <div className="flex flex-col w-full ml-48">
-          <div className="bg-gray-100  text-right ">
-            <p>Abdul Manan</p>
-            <p>abdulmzak36@gmail.com</p>
+          <div className="bg-gray-100  text-right font-sans">
+            <p>Hassan Sardar</p>
+            <p>hassansrdar626@gmail.com</p>
           </div>
           <div className=" w-full ">
             <div className="  pt-24 w-full  flex items-center ">
-              <div className="bg-gray-100 p-5  w-3/4 ">
+              <div className="bg-gray-100 p-5  w-3/4 font-sans">
                 <p>
                   <b>Good Morning!</b>
                 </p>
-                <b> Abdul Manan </b>
+                <b>Hassan Sardar</b>
                 <p>Remember every good conversation starts with listening..</p>
               </div>
 
@@ -59,7 +59,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="pl-5 pt-10 font-sans md:font-serif">
+          <div className="pl-5 pt-10 font-sans md:font-sans">
             <b>Recent Events</b>
           </div>
           <video muted loop autoPlay className="rounded-lg  m-10 ">
@@ -70,7 +70,7 @@ function App() {
             ></source>
           </video>
           <div className="pl-5 pt-6 font-sans md:font-serif">
-            <b>Upcoming Meetings</b>       <button className=" text-zinc-50 rounded
+            <b>Upcoming Meetings</b>       <button className="p-2 text-zinc-50 rounded
              bg-slate-500">View All</button>
             <p className="pt-5 pl-14">No Schedule Meeting</p>
           </div>
@@ -104,8 +104,8 @@ function App() {
               </div>
             </div>
             
-          <div className="pl-5 pt-6 font-sans md:font-serif">
-            <b>Current Meetings</b>       <button className=" text-zinc-50 rounded
+          <div className="pl-5 pt-28 font-sans md:font-serif">
+            <b>Current Meetings</b>       <button className="p-2 text-zinc-50 rounded
              bg-slate-500">View All</button>
              <div className=" w-full ">
             <div className="  pt-12 w-full  flex items-center ">
