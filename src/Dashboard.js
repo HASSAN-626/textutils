7e545633e18b2c9348e680ac6de39ad63542fa0e
HASSAN-626 <hassansrdar626@gmail.com>
2024-07-18 13:11:41 +0500

    modified

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,30 +5,27 @@ export default function Dashboard(props) {
     <>
       <div className="w-48 py-8 h-screen border border-gray-900 fixed left-0 font-sans md:font-serif flex flex-col items-center ">
         <img src="./download.jpg" alt="" width={85} height={85} />
-        <AsideButton text={"Dashboard"}/>
+        <AsideButton text={"Dashboard"} />
         <AsideButton text={"Personal"} />
         <AsideButton text={"My Rota"} />
         <AsideButton text={"Inbox"} />
         <AsideButton text={"c2o Profile"} />
         <AsideButton text={"Chats"} />
         <AsideButton text={"ATS"} />
-        <AsideButton text={"CMS"}/>
+        <AsideButton text={"CMS"} />
         <AsideButton text={"OSCS"} />
-        <AsideButton text={"Preference"} />
+        <AsideButton text={"Preference"} border />
       </div>
     </>
   );
 }
-function AsideButton({ text }) {
+
+function AsideButton({ text, border = true }) {
   return (
     <a
       href=""
       className={`p-4 font-semibold flex justify-center w-full ${
-        text == "Dashboard"
-          ? " border-2 "
-          : text == "CMS"
-          ? "border-2"
-          : ""
+        border ? "border-red-700 border" : ""
       } `}
     >
       <p>{text}</p>
