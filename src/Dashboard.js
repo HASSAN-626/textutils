20d126bcde50432067648440945935d30abe78e1
HASSAN-626 <hassansrdar626@gmail.com>
2024-07-19 12:17:46 +0500

    modified

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -16,23 +16,23 @@ export default function Dashboard(props) {
   let showArray = [];
   for (let i = 0; i <= arr.length; i++) {
     let border = false;
-    if (arr[i]=="Dashboard") { 
+    if (arr[i] == "Dashboard") {
       border = true;
     }
     showArray.push(<AsideButton text={arr[i]} border={border} />);
   }
   return (
     <>
-      <div className="w-48 py-8 h-screen border border-gray-900 fixed left-0 font-sans md:font-serif
-       flex flex-col items-center">
+      <div
+        className="w-48 py-8 h-screen border border-gray-900 fixed left-0 font-sans md:font-serif
+       flex flex-col items-center"
+      >
         <img src="./download.jpg" alt="" width={85} height={85} />
         {showArray}
-        
       </div>
     </>
   );
 }
-
 function AsideButton({ text, border = false }) {
   return (
     <a
