3069b332e8d33d669bd4465b9438e23e831b77ff
HASSAN-626 <hassansrdar626@gmail.com>
2024-07-24 11:40:09 +0500

    modifier

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { IconLogout } from "@tabler/icons-react";
 export default function Nav() {
   return (
     <>
-    <div className="bg-gray-100  text-right ">
-              <p>Hassan Sardar</p>
-              <p>hassansrdar626@gmail.com</p>
+      <div className="w-75 pl-148   ">
+        <div className="pr-8  ml-72 mr-10 -mb-11 rounded-xl mt-4 bg-slate-200 pl-6 flex-col">
+          <div className="flex items-center">
+            <div>
+              <p className="font-medium">Hassan Sardar</p>
+              <p className="text-black-light">hassansrdar626@gmail.com</p>
+            </div> &nbsp;&nbsp;&nbsp;
+            <div>
+              <p>│</p>
+            </div>&nbsp;&nbsp;
+            <div className="items-end">
+              <p>
+                <IconLogout/>
+              </p>
             </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
