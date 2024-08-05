5b37a1eec4290a9dd048762050670971c28ac2c5
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-05 13:21:58 +0500

    Update Pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 export default function Personal() {
   return (
-    <div className="bg-red-500">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing 
-        elit. Aperiam repudiandae eveniet ducimus dolor 
-        excepturi natus voluptatibus voluptate eos nemo.
-         Et quo officiis illo, nam iste quibusdam
-        # exercitationem impedit aspernatur asperiores.
-      </p>
+    <>
+    <div className="  pt-2 ml-4 w-full  flex ">
+      <div className="pt-10 ml-4 bg-white p-5  w-3/4 border-2 rounded-lg shadow-md">
+        <b>Testing Company</b>
+        <div className="flex">
+          <div className="flex-col">
+            <p className="pt-2">Status:Meeting</p>
+            <p>Joining Date:&nbsp;5/28/2024</p>
+          </div>
 
-      
+          <div className=" pl-56 flex-col font-normal text-black">
+            <p className="pt-2">Asif Wani</p>
+            <p>testingcompany@gmail.com</p>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
