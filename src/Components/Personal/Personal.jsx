9d14c467e34c98f5d6ed3f8248c4fac460c222d6
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-06 13:22:55 +0500

    Update Pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -3,30 +3,83 @@ export default function Personal() {
   return (
     <>
       <div className="pt-4 ml-4">
-        <div className="pt-10 ml-4 bg-white p-5  w-full border-2 rounded-lg shadow-md">
+        <div className=" ml-4 bg-white p-5  w-full border-2 rounded-lg shadow-md">
           <p className="text-black font-semibold">PERSONAL INFORMATION</p>
-          <div className="flex">
-            
-            
-            <div className=" pl-56 flex-col font-normal text-black">
-              <p className="pt-2">Asif Wani</p>
-              <p>testingcompany@gmail.com</p>
+          <div className="pt-2 grid grid-rows-4 grid-flow-col ">
+            <div>
+              <p className="text-black-light font-semibold">Joining Date</p>
+              <p>2024-06-24</p>
             </div>
 
-            <div className=" pl-56 flex-col font-normal text-black">
-              <p className="pt-2">Asif Wani</p>
-              <p>testingcompany@gmail.com</p>
+            <div>
+              <p className="text-black-light font-semibold">Email</p>
+              <p>hassansrdar626@gmail.com</p>
             </div>
 
-            <div className=" pl-56 flex-col font-normal text-black">
-              <p className="pt-2">Asif Wani</p>
-              <p>testingcompany@gmail.com</p>
-            </div>           
-             </div>
+            <div>
+              <p className="text-black-light font-semibold">Gender</p>
+              <p>Male</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">
+                Parent/Guardian Phone No
+              </p>
+              <p>03325146837</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Date Of Birth</p>
+              <p>2004-05-09</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Phone Number</p>
+              <p>03165105988</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Blood Group</p>
+              <p>O+</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Parent/Guardian</p>
+              <p>37405-39969996-4</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Martial Status</p>
+              <p>Single</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">CNIC Number</p>
+              <p>37405-4563494-1</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">
+                Parent/Guardian Name
+              </p>
+              <p>Riffat Samina</p>
+            </div>
+
+            <div>
+              <p className="text-black-light font-semibold">Address</p>
+              <p>
+                Haroown chowk , Kurri Road , Near Hasnain Shopping Mall,
+                <p>Rawalpindi</p>
+              </p>
+            </div>
+
+            <div>
+            <img src="" alt="" />
+
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 }
-F
