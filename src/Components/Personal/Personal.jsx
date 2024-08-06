753204492cdfcbb4d172e5056305274ed5a5e9b5
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-06 11:25:04 +0500

    Update Pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -2,26 +2,31 @@ import React from "react";
 export default function Personal() {
   return (
     <>
-      <div className="  pt-4 ml-4   flex ">
+      <div className="pt-4 ml-4">
         <div className="pt-10 ml-4 bg-white p-5  w-full border-2 rounded-lg shadow-md">
-          <b>Personal Information</b>
+          <p className="text-black font-semibold">PERSONAL INFORMATION</p>
           <div className="flex">
-            <div className="flex-col pt-2">
-              Joining Date :
-              24th June 2024
-                 
-                 
-                 
-                 </div>
             
+            
+            <div className=" pl-56 flex-col font-normal text-black">
+              <p className="pt-2">Asif Wani</p>
+              <p>testingcompany@gmail.com</p>
+            </div>
 
             <div className=" pl-56 flex-col font-normal text-black">
               <p className="pt-2">Asif Wani</p>
               <p>testingcompany@gmail.com</p>
             </div>
+
+            <div className=" pl-56 flex-col font-normal text-black">
+              <p className="pt-2">Asif Wani</p>
+              <p>testingcompany@gmail.com</p>
+            </div>           
+             </div>
           </div>
         </div>
       </div>
     </>
   );
 }
+F
