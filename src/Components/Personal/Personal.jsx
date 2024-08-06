ebc559f349c4c6aeeeb588b129c188df1a2835fe
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-06 15:06:50 +0500

    Udate Pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -3,79 +3,90 @@ export default function Personal() {
   return (
     <>
       <div className="pt-4 ml-4">
-        <div className=" ml-4 bg-white p-5  w-full border-2 rounded-lg shadow-md">
-          <p className="text-black font-semibold">PERSONAL INFORMATION</p>
-          <div className="pt-2 grid grid-rows-4 grid-flow-col ">
-            <div>
-              <p className="text-black-light font-semibold">Joining Date</p>
-              <p>2024-06-24</p>
-            </div>
+        <div className=" ml-4 bg-white p-4  w-full border-2 rounded-lg shadow-md ">
+        <p className="text-black font-semibold">PERSONAL INFORMATION</p>
+          <div className="flex items-center ">
+            <div className="pt-2 grid grid-rows-4 grid-flow-col ">
+              <div>
+                <p className="text-black-light font-semibold">Joining Date</p>
+                <p>2024-06-24</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Email</p>
-              <p>hassansrdar626@gmail.com</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Email</p>
+                <p>hassansrdar626@gmail.com</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Gender</p>
-              <p>Male</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Gender</p>
+                <p>Male</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">
-                Parent/Guardian Phone No
-              </p>
-              <p>03325146837</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">
+                  Parent/Guardian Phone No
+                </p>
+                <p>03325146837</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Date Of Birth</p>
-              <p>2004-05-09</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Date Of Birth</p>
+                <p>2004-05-09</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Phone Number</p>
-              <p>03165105988</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Phone Number</p>
+                <p>03165105988</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Blood Group</p>
-              <p>O+</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Blood Group</p>
+                <p>O+</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Parent/Guardian</p>
-              <p>37405-39969996-4</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">
+                  Parent/Guardian
+                </p>
+                <p>37405-39969996-4</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Martial Status</p>
-              <p>Single</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">Martial Status</p>
+                <p>Single</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">CNIC Number</p>
-              <p>37405-4563494-1</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">CNIC Number</p>
+                <p>37405-4563494-1</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">
-                Parent/Guardian Name
-              </p>
-              <p>Riffat Samina</p>
-            </div>
+              <div>
+                <p className="text-black-light font-semibold">
+                  Parent/Guardian Name
+                </p>
+                <p>Riffat Samina</p>
+              </div>
 
-            <div>
-              <p className="text-black-light font-semibold">Address</p>
-              <p>
-                Haroown chowk , Kurri Road , Near Hasnain Shopping Mall,
-                <p>Rawalpindi</p>
-              </p>
-            </div>
-
-            <div>
-            <img src="" alt="" />
-
+              <div>
+                <p className="text-black-light font-semibold">Address</p>
+                <p>
+                  Haroown chowk , Kurri Road , Near Hasnain Shopping Mall,
+                  <p>Rawalpindi</p>
+                </p>
+              </div>
+              </div>
+              <div>
+                <img
+                  src="./58a434a3-826c-4c3a-98cc-a818b83def59.jpeg"
+                  className="h-38 w-28 object-cover rounded-xl"
+                  alt=""
+                />
+                <p className="text-black-light font-semibold">Hassan Sardar</p>
+                <p className="text-black-lighter font-semibold text-sm pl-2">
+                  IT - Web Dept
+                </p>
+                <p className="text-black-lighter  text-sm pl-5">(Active)</p>
             </div>
           </div>
         </div>
@@ -83,3 +94,5 @@ export default function Personal() {
     </>
   );
 }
+
+// className="h-64 w-32 object-cover rounded"
