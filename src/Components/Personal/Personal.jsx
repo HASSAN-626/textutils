15c9b51e992a2611df1bfb59a1ac5bf96222aab0
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-07 13:01:06 +0500

    Update Pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -162,20 +162,41 @@ export default function Personal() {
             <div className="flex h-12 p-3 pr-36  bg-white rounded-2xl shadow-md text-lg text-black-lighter ">
               <img
                 src="./79cd3aed-63d8-4c5f-87fe-bb360eb094d8.jpeg"
-                width={'32'}
-                height={'auto'}
+                width={"32"}
+                height={"auto"}
                 alt=""
               />
               <p className="pl-4 font-semibold ">3. c2o Management</p>
             </div>
             <div className="pr-28">
               <p className="text-black font-semibold">COMPANY DETAILS</p>
-              <p className="text-sm text-black-light">Department &nbsp;&nbsp;Web Department</p>
-              <p className="text-sm text-black-light">Wage/Salary &nbsp;&nbsp;---</p>
-              <p className="text-sm text-black-light">Job Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Trainee</p>
+              <p className="text-sm text-black-light">
+                Department &nbsp;&nbsp;Web Department
+              </p>
+              <p className="text-sm text-black-light">
+                Wage/Salary &nbsp;&nbsp;---
+              </p>
+              <p className="text-sm text-black-light">
+                Job Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Trainee
+              </p>
             </div>
           </div>
           <div className="mt-3 w-full border border- mx-2 "></div>
+          <p className="text-black font-semibold pt-4">DOCUMENTS</p>
+          <div className="flex-col">
+            <div className="flex justify-between w-full m-2 p-2 rounded-lg bg-gray-200 ">
+              <p className="text-m text-black-light">Name</p>
+              <p className="pr-56 text-m text-black-light">Download</p>
+            </div>
+            <div className="flex justify-between w-full m-2 p-2 rounded-lg bg-gray-100 ">
+              <p className="text-m text-black-light">CNIC front</p>
+              <p className="pr-56 text-m text-black-light">Download</p>
+            </div>
+            <div className="flex justify-between w-full m-2 p-2 rounded-lg bg-gray-100 hover-bg-gray-200 ">
+              <p className="text-m text-black-light">CNIC back</p>
+              <p className="pr-56 text-m text-black-light">Download</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
