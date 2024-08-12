41a144ba7ba632b1f310fb31964a7f2094bcb438
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-12 12:16:58 +0500

    update pro

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -581,9 +581,9 @@ export default function Personal() {
                     </div>
                     <div className="mt-4 mb-3 pr-2 w-full border border- "></div>
                     <div className="flex items-end justify-end gap-8">
-                     <button className="bg-blue-100 p-1 rounded-md text-blue-600 pl-10 pr-10">SAVE & NEXT</button>
-                     <button className="bg-purple-100 p-1 rounded-md text-purple-600 pl-10 pr-10">NEXT</button>
-                      <button className="bg-green-100 p-1 rounded-md text-green-600">SAVE & EXIT</button>
+                     <button className="bg-blue-100 p-1 rounded-md text-blue-700 pl-10 pr-10 font-semibold">SAVE & NEXT</button>
+                     <button className="bg-purple-100 p-1 rounded-md text-purple-700 pl-10 pr-10 font-semibold ">NEXT</button>
+                      <button className="bg-green-100 p-1 rounded-md text-green-700 pl-10 pr-10 font-semibold">SAVE & EXIT</button>
                     </div>
                   </div>
                 </form>
