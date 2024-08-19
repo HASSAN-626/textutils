c841fbdaa61b01332ed0adc81616285e3eb88d9a
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-19 11:46:24 +0500

    Update

--- src/Components/Personal/Modal.jsx
+++ src/Components/Personal/Modal.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { IconUserCircle } from "@tabler/icons-react";
+import {IconFileDescription} from "@tabler/icons-react";
+import {IconId} from "@tabler/icons-react";
+import {IconKey} from "@tabler/icons-react";
 const Modal = ({ isVisible, onClose,children }) => {
   if (!isVisible) return null;
   return (
@@ -8,6 +11,12 @@ const Modal = ({ isVisible, onClose,children }) => {
         className="fixed inset-0 bg-black bg-opacity-55 backdrop-blur-md
     flex justify-center items-center"
       >
+      <div className="flex-col mb-80 bg-slate-200 p-1 rounded-md">
+      <button className="pt-2 pb-5 hover:text-primary "><IconUserCircle/></button> <br />
+      <button className="pb-5 hover:text-primary"><IconId/></button> <br />
+      <button className="pb-5 hover:text-primary"><IconFileDescription/></button> <br />
+      <button className="pb-5 hover:text-primary"><IconKey /></button> 
+      </div>
         <div className="w-2/4 flex flex-col">
           <button
             className="place-self-end text-white text-lg "
