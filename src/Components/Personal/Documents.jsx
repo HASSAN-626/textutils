6eabd483b6aeb67c83b9fd812e7d7a779f1f93fd
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-20 11:07:31 +0500

    update pro

--- src/Components/Personal/Documents.jsx
+++ src/Components/Personal/Documents.jsx
@@ -16,19 +16,19 @@ export default function Documents() {
       >
         <p className="text-black-light pb-2">EMPLOYEE DOCUMENTS</p>
         <div className="mt-5 flex items-end justify-end gap-5">
-          <button className="bg-blue-100 p-2 rounded-md text-blue-700 pl-10 pr-10 font-medium hover:opacity-55">
+          <button className="bg-blue-100 p-2 rounded-md text-blue-700 pl-9 pr-9 font-medium hover:opacity-55">
             SAVE & NEXT
           </button>
-          <button className="bg-purple-100 p-2 rounded-md text-purple-700 pl-10 pr-10 font-medium hover:opacity-55">
+          <button className="bg-purple-100 p-2 rounded-md text-purple-700 pl-9 pr-9 font-medium hover:opacity-55">
             NEXT
           </button>
-          <button className="bg-green-100 p-2 rounded-md text-green-700 pl-10 pr-10 font-medium hover:opacity-55">
+          <button className="bg-green-100 p-2 rounded-md text-green-700 pl-9 pr-9 font-medium hover:opacity-55">
             SAVE & EXIT
           </button>
         </div>
-        <div className="flex justify-between w-96 m-1 p-2 rounded-lg bg-gray-100  hover:bg-gray-200 ">
+            <div className="flex justify-between w-112 m-1 p-2 rounded-lg bg-gray-100  hover:bg-gray-200 ">
           <p className="text-m text-black-light">Name</p>
-          <p className="pr-16 text-m text-black-light">
+          <p className="pr-11 text-m text-black-light">
             <div className=" hover:bg-slate-300 rounded-sm ">Action</div>
           </p>
         </div>
