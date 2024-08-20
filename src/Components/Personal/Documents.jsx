186e8966ed6dc7e6ec2de75a53b6d3e6a746d460
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-20 16:49:36 +0500

    save

--- src/Components/Personal/Documents.jsx
+++ src/Components/Personal/Documents.jsx
@@ -1,23 +1,31 @@
-import React from "react";
-import { IconUser } from "@tabler/icons-react";
-import { IconHelpHexagon } from "@tabler/icons-react";
+import React, { useRef } from "react";
+import { IconUser, IconHelpHexagon } from "@tabler/icons-react";
+
 export default function Documents() {
+
+  const fileInputRef = useRef(null);
+
+
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <form action="">
-      <p className="pl-3 pb-5 text-black font-semibold flex h-4  ">
+      <p className="pl-3 pb-5 text-black font-semibold flex h-4">
         <IconUser width={"20"} height={"20"} />
         &nbsp; EDIT HASSAN SARDAR - DOCUMENTS&nbsp;
         <IconHelpHexagon width={"20"} height={"20"} />
       </p>
-      <div className="mt-4 mb-3 pr-2 w-full border border-x-black-lighter "></div>
+      <div className="mt-4 mb-3 pr-2 w-full border border-x-black-lighter"></div>
       <div
-        className="p-2 h-60vh overflow-auto  justify-between"
+        className="p-2 h-60vh overflow-auto justify-between"
         style={{
           scrollbarWidth: "thin",
         }}
       >
         <p className="text-black-light pb-1">EMPLOYEE DOCUMENTS</p>
-        <div className=" flex items-end justify-end gap-5">
+        <div className="flex items-end justify-end gap-5">
           <button className="bg-blue-100 p-2 rounded-md text-blue-700 pl-9 pr-9 font-medium hover:opacity-55">
             SAVE & NEXT
           </button>
@@ -28,38 +36,38 @@ export default function Documents() {
             SAVE & EXIT
           </button>
         </div>
-        <div className="flex justify-between  w-96 m-1 p-2 rounded-lg bg-gray-200  hover:bg-gray-200 ">
+        <div className="flex justify-between w-96 m-1 p-2 rounded-lg bg-gray-200 hover:bg-gray-200">
           <p className="pl-2 text-m text-black-lighter">Name</p>
           <p className="pr-2 text-m text-black-light">
-            <div className=" hover:bg-slate-300 rounded-sm ">Actions</div>
+            <div className="hover:bg-slate-300 rounded-sm">Actions</div>
           </p>
         </div>
-        <div className="mt-2 flex justify-between  w-96 m-1 p-2 rounded-lg bg-gray-100  hover:bg-gray-200 ">
+        <div className="mt-2 flex justify-between w-96 m-1 p-2 rounded-lg bg-gray-100 hover:bg-gray-200">
           <p className="pl-2 text-m text-black-light font-thin">CNIC Front</p>
           <p className="pr-2 text-m text-black-light">
-            <div className=" hover:bg-slate-300 rounded-sm text-red-600 pl-1 pr-1">
+            <div className="hover:bg-slate-300 rounded-sm text-red-600 pl-1 pr-1">
               X
             </div>
           </p>
         </div>
-        <div className="mt-2 flex justify-between  w-96 m-1 p-2 rounded-lg bg-gray-100  hover:bg-gray-200 ">
+        <div className="mt-2 flex justify-between w-96 m-1 p-2 rounded-lg bg-gray-100 hover:bg-gray-200">
           <p className="pl-2 text-m text-black-light font-thin">CNIC back</p>
           <p className="pr-2 text-m text-black-light">
-            <div className=" hover:bg-slate-300 rounded-sm text-red-600 pl-1 pr-1">
+            <div className="hover:bg-slate-300 rounded-sm text-red-600 pl-1 pr-1">
               X
             </div>
           </p>
         </div>
 
-        <div className="mt-2 flex justify-between  w-96 m-1 p-2 rounded-lg bg-gray-300  ">
+        <div className="mt-2 flex justify-between w-96 m-1 p-2 rounded-lg bg-gray-300">
           <p className="pl-2 text-m text-black-light text-sm">
             Document Type <br />
             <select
               name="title"
               id=""
-              className="p-1 mt-1  bg-gray-200 rounded-lg pl-1 w-48 font-medium  border hover:border-black"
+              className="p-1 mt-1 bg-gray-200 rounded-lg pl-1 w-48 font-medium border hover:border-black"
             >
-              <option value="text" className="bg-slate-20 ">
+              <option value="text" className="bg-slate-20">
                 Phd
               </option>
               <option className="bg-slate-200">Master</option>
@@ -74,9 +82,21 @@ export default function Documents() {
             </select>
           </p>
           <p className="pr-2 text-m text-black-light">
-            <button className="hover:opacity-80 rounded-md p-1 mt-3 pl-3 pr-3 bg-primary text-slate-200 font-thin">
+            <button
+              type="button" 
+              onClick={handleUploadClick}
+              className="hover:opacity-80 rounded-md p-1 mt-3 pl-3 pr-3 bg-primary text-slate-200 font-thin"
+            >
               Upload
             </button>
+            <input
+              type="file"
+              ref={fileInputRef}
+              style={{ display: 'none' }} 
+              onChange={(e) => {
+                console.log(e.target.files);
+              }}
+            />
           </p>
         </div>
       </div>
