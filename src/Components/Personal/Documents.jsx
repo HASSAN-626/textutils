17843e8a0bc2d6a1cc243b34deb8886256bcaaf1
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-20 10:51:15 +0500

    update

--- src/Components/Personal/Documents.jsx
+++ src/Components/Personal/Documents.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Documents() {
   return (
-    <div>
-      
-    </div>
-  )
+    <form action="">
+      <p className="pl-3 pb-5 text-black font-semibold flex h-4  ">
+        <IconUser width={"20"} height={"20"} />
+        &nbsp; EDIT HASSAN SARDAR - DETAILS
+      </p>
+      <div className="mt-4 mb-3 pr-2 w-full border border-x-black-lighter "></div>
+      <div
+        className="p-2 h-60vh overflow-auto  "
+        style={{
+          scrollbarWidth: "thin",
+        }}
+      >
+        <p className="text-black-light pb-2">EMPLOYEE DETAILS</p>
+      </div>
+    </form>
+  );
 }
