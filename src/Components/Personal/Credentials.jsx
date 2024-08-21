fa38134f2b015aee35f205607138f9c937f833cf
HASSAN-626 <hassansrdar626@gmail.com>
2024-08-21 10:34:23 +0500

    save

--- src/Components/Personal/Credentials.jsx
+++ src/Components/Personal/Credentials.jsx
@@ -13,19 +13,19 @@ export default function Credentials() {
 
   return (
     <form action="">
-      <p className="pl-3 pb-5 text-black font-semibold flex h-4">
+      <p className="pl-3 pb-5 text-black font-semibold flex h-4 items-center">
         <IconUser width={"20"} height={"20"} />
         &nbsp; EDIT HASSAN SARDAR - CREDENTIALS &nbsp;{" "}
         <IconHelpHexagon width={"20"} height={"20"} />
       </p>
       <div className="mt-4 mb-3 pr-2 w-full border border-x-black-lighter"></div>
       <div
-        className="p-2 h-60vh overflow-auto justify-between"
+        className="p-2 h-60vh overflow-auto"
         style={{ scrollbarWidth: "thin" }}
       >
         <p className="text-black-light pb-1">CHANGE PIN</p>
-        <div className="grid grid-rows-1 grid-flow-col mt-2 ">
-          <div className="relative w-64">
+        <div className="grid grid-cols-2 gap-4 mt-2">
+          <div className="relative w-full">
             <label htmlFor="newpin" className="text-sm text-black-light">
               New Pin
             </label>
@@ -35,7 +35,7 @@ export default function Credentials() {
                 type={showNewPin ? "text" : "password"}
                 name="newpin"
                 placeholder=""
-                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-10"
+                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-12"
               />
               <button
                 type="button"
@@ -46,7 +46,7 @@ export default function Credentials() {
               </button>
             </div>
           </div>
-          <div className="relative w-64">
+          <div className="relative w-full">
             <label htmlFor="confirmnewpin" className="text-sm text-black-light">
               Confirm New Pin
             </label>
@@ -56,7 +56,7 @@ export default function Credentials() {
                 type={showConfirmNewPin ? "text" : "password"}
                 name="confirmnewpin"
                 placeholder=""
-                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-10"
+                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-12"
               />
               <button
                 type="button"
@@ -70,8 +70,8 @@ export default function Credentials() {
         </div>
         <div className="mt-6 mb-3 pr-2 w-full border border-x-black"></div>
         <p className="text-black-light pb-1">CHANGE PASSWORD</p>
-        <div className="grid grid-rows-1 grid-flow-col mt-2 gap-4">
-          <div className="relative w-64">
+        <div className="grid grid-cols-2 gap-4 mt-2">
+          <div className="relative w-full">
             <label htmlFor="currentpassword" className="text-sm text-black-light">
               Current Password
             </label>
@@ -81,7 +81,7 @@ export default function Credentials() {
                 type={showCurrentPassword ? "text" : "password"}
                 name="currentpassword"
                 placeholder=""
-                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-10"
+                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-12"
               />
               <button
                 type="button"
@@ -92,7 +92,7 @@ export default function Credentials() {
               </button>
             </div>
           </div>
-          <div className="relative w-64">
+          <div className="relative w-full">
             <label htmlFor="newpassword" className="text-sm text-black-light">
               New Password
             </label>
@@ -102,7 +102,7 @@ export default function Credentials() {
                 type={showNewPassword ? "text" : "password"}
                 name="newpassword"
                 placeholder=""
-                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-10"
+                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-12"
               />
               <button
                 type="button"
@@ -113,7 +113,7 @@ export default function Credentials() {
               </button>
             </div>
           </div>
-          <div className="relative w-64">
+          <div className="relative w-full">
             <label htmlFor="confirmnewpassword" className="text-sm text-black-light">
               Confirm New Password
             </label>
@@ -123,7 +123,7 @@ export default function Credentials() {
                 type={showNewPassword ? "text" : "password"}
                 name="confirmnewpassword"
                 placeholder=""
-                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-10"
+                className="mt-1 p-2 bg-slate-200 rounded-lg border w-full pr-12"
               />
               <button
                 type="button"
