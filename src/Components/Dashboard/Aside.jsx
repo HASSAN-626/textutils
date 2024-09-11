b449b39e5e1bd457223164f6d7d0ad48a91e5fed
HASSAN-626 <hassansrdar626@gmail.com>
2024-09-11 12:32:57 +0500

    save

--- src/Components/Dashboard/Aside.jsx
+++ src/Components/Dashboard/Aside.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IconLayoutDashboard } from "@tabler/icons-react";
 import { IconUser } from "@tabler/icons-react";
 import { IconSquareRotated } from "@tabler/icons-react";
@@ -12,7 +13,9 @@ import { IconLayersDifference } from "@tabler/icons-react";
 import React from "react";
 
 export default function Aside({ setActiveComponent }) {
-  
+  // Maintain the state of the active component
+  const [activeComponent, setActiveComponentState] = useState("dashboard");
+
   const arr = [
     { title: "Dashboard", Icon: IconLayoutDashboard, component: "dashboard" },
     { title: "Personal", Icon: IconUser, component: "personal" },
@@ -26,17 +29,11 @@ export default function Aside({ setActiveComponent }) {
     { title: "Preference", Icon: IconLayersDifference, component: "preference" },
   ];
 
-  let showArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    showArray.push(
-      <AsideButton 
-        key={i} 
-        title={arr[i].title} 
-        Icon={arr[i].Icon} 
-        onClick={() => setActiveComponent(arr[i].component)} 
-      />
-    );
-  }
+  const handleClick = (component) => {
+    setActiveComponent(component);
+    setActiveComponentState(component); // Update the state with the clicked component
+  };
+
   return (
     <div
       className="w-48 py-6 h-screen fixed left-0 
@@ -49,127 +46,28 @@ export default function Aside({ setActiveComponent }) {
         height={140}
         className="mt-6"
       />
-      {showArray}
+      {arr.map(({ title, Icon, component }) => (
+        <AsideButton 
+          key={component} 
+          title={title} 
+          Icon={Icon} 
+          isActive={activeComponent === component} // Pass active state
+          onClick={() => handleClick(component)} 
+        />
+      ))}
     </div>
   );
 }
 
-function AsideButton({ title, Icon, onClick }) {
+function AsideButton({ title, Icon, isActive, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="p-3 pl-8 mt-1 font-semibold flex w-full hover:bg-primary hover:text-white hover:rounded-md cursor-pointer"
+      className={`p-3 pl-8 mt-1 font-semibold flex w-full cursor-pointer
+        ${isActive ? 'bg-primary text-white rounded-md' : 'hover:bg-primary hover:text-white'}`}
     >
       <Icon className="mr-2 " />
       <p>{title}</p>
     </div>
   );
 }
-
-
-//     <><div className="bg-white">
-//       <div
-//         className="w-48  py-6 h-full   fixed left-0
-//        flex flex-col items-center text-slate-700 rounded-lg text-lg bg-white"
-//       >
-//         <img src="./09261924-a421-4778-be9b-f359c690718f.jpeg" alt="" width={140} height={140} className="mt-6"/>
-//         <div className="mt-8">
-//         <a href="" className="p-3 font-semibold flex   hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconLayoutDashboard />&nbsp;
-//           Dashboard
-//         </a>
-//         <a href="" className="p-4 font-semibold flex hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconUser />&nbsp;
-//           Personal
-//         </a>
-//         <a href="" className="p-4 font-semibold flex   hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconSquareRotated />&nbsp;
-//            My Rota
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconMessages />&nbsp;
-//            Inbox
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconUserScan />&nbsp;
-//            c2o Profile
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconBrandHipchat />&nbsp;
-//            Chats
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconAssembly />&nbsp;
-//           ATS
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconAbacus />&nbsp;
-//            CMS
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconFocus />&nbsp;
-//           OCCS
-//         </a>
-//         <a href="" className="p-4 font-semibold flex  hover:bg-slate-400 hover:text-white hover:rounded-lg">
-//           <IconLayersDifference />&nbsp;
-//           Preferences
-//         </a>
-//         </div>
-//       </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// import { IconLayoutDashboard } from "@tabler/icons-react";
-// import { IconUser } from "@tabler/icons-react";
-// import React from "react";
-
-// export default function Dashboard(props) {
-//   let arr = [
-//     "Dashboard",
-//     "Personal",
-//     "My Rota",
-//     "Inbox",
-//     "c2o Profile",
-//     "Chats",
-//     "ATS",
-//     "CMS",
-//     "OSCS",
-//     "Preference",
-//   ];
-//   let showArray = [];
-//   for (let i = 0; i <= arr.length; i++) {
-//     let border = false;
-//     if (arr[i] == "Dashboard") {
-//       border = true;
-
-//     }
-//     showArray.push(<AsideButton text={arr[i]} border={border} />);
-//   }
-//   return (
-//     <>
-//       <div
-//         className="w-48 py-8 h-screen  border border-gray-900 fixed left-0 font-sans md:font-serif
-//        flex flex-col items-center"
-//       >
-//         <img src="./download.jpg" alt="" width={85} height={85} />
-//         <IconLayoutDashboard />
-//         <IconUser/>
-//         {showArray}
-//       </div>
-//     </>
-//   );
-// }
-// function AsideButton({ text, border = false }) {
-//   return (
-//     <a
-//       href=""
-//       className={`p-4 font-semibold flex justify-center w-full ${
-//         border ? "border-red-900 border" : ""
-//       } `}
-//     >
-//       <p>{text}</p>
-//     </a>
-//   );
-// }
